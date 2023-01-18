aef23f9c1d2a8c6ac2c888a98df8f453f19f3abb
a-shiro <alekmitov20@gmail.com>
2023-01-18 11:55:04 +0200

    Practicing components

--- yt-course/src/App.js
+++ yt-course/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <User name="Shira" age={21} email="shira@gmail.com" />
+      <User name="Shiro" age={22} email="shiro@gmail.com" />
+      <User name="Peter" age={29} email="peter@gmail.com" />
     </div>
   );
 }
+
+const User = (props) => {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <h1>{props.age}</h1>
+      <h1>{props.email}</h1>
+    </div>
+  );
+}
+
 
 export default App;
