3cf0d89d23c53102c459050c1bf4e65f6f5b41bd
a-shiro <alekmitov20@gmail.com>
2023-01-19 11:55:17 +0200

    Refactored Code

--- yt-course/src/App.js
+++ yt-course/src/App.js
@@ -1,50 +1,16 @@
+import { User } from './components/User';
+import { Job } from './components/Job';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <User name="John" age={21} email="john@gmail.com" />
-      <Job salary={90000} position="Junior Front-End" complany="Amazon" />
-      <Hobby hobby="Fishing" />
+      <User name="Shiro" age={21} email="shiro@gmail.com" />
 
-      <User name="Shiro" age={22} email="shiro@gmail.com" />
-      <Job salary={120000} position="Senior Full Stack" complany="Google" />
-      <Hobby hobby="Video Games" />
-
-      <User name="Peter" age={29} email="peter@gmail.com" />
-      <Job salary={160000} position="Project Manager" complany="<Microsoft>" />
-      <Hobby hobby="Swimming" />
+      <Job salary={90000} position="Junior Front-End" company="Google" />
     </div>
   );
 }
-
-const User = (props) => {
-  return (
-    <div>
-      <h1>{props.name}</h1>
-      <h1>{props.age}</h1>
-      <h1>{props.email}</h1>
-    </div>
-  );
-}
-
-const Job = (props) => {
-  return (
-    <div>
-      <h1>{props.salary}</h1>
-      <h1>{props.position}</h1>
-      <h1>{props.complany}</h1>
-    </div>
-  )
-}
-
-const Hobby = (props) => {
-  return (
-    <div>
-      <h2>{props.hobby}</h2>
-    </div>
-  )
-}
-
 
 export default App;
