5c1ef6d4013cb53789e8ea9af82daa3877500cad
a-shiro <alekmitov20@gmail.com>
2023-01-25 18:36:31 +0200

    Added Styling

--- exercises/src/App.js
+++ exercises/src/App.js
@@ -4,32 +4,38 @@ import { ExerciseSection } from "./components/ExerciseSection";
 function App() {
   return (
     <div>
-      <label>Select Exercise</label>
-      <select
-        onChange={(event) => {
-          document.querySelectorAll("section").forEach((section) => {
-            section.className = "";
-            section.classList.add("hidden");
-          });
+      <div className="label-box">
+        <label>Select Exercise</label>
+        <select
+          onChange={(event) => {
+            document.querySelectorAll("section").forEach((section) => {
+              section.className = "";
+              section.classList.add("hidden");
+            });
 
-          const section = document.getElementById(event.target.value);
-          const visibility = section.classList[0];
+            const section = document.getElementById(event.target.value);
+            const visibility = section.classList[0];
 
-          section.classList.remove(visibility);
-          section.classList.add(visibility === "hidden" ? "visible" : "hidden");
-        }}
-      >
-        <option>---</option>
-        <option>1</option>
-        <option>2</option>
-        <option>3</option>
-        <option>4</option>
-        <option>5</option>
-        <option>6</option>
-        <option>7</option>
-      </select>
+            section.classList.remove(visibility);
+            section.classList.add(
+              visibility === "hidden" ? "visible" : "hidden"
+            );
+          }}
+        >
+          <option>---</option>
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+          <option>6</option>
+          <option>7</option>
+        </select>
+      </div>
 
-      <ExerciseSection />
+      <div className="exercise-box">
+        <ExerciseSection />
+      </div>
     </div>
   );
 }
