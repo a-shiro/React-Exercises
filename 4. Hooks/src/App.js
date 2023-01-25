ee58bc4ff49171e5318b63f4970ff1cafa41ae90
a-shiro <alekmitov20@gmail.com>
2023-01-25 11:57:06 +0200

    Added Todo List Exercise

--- 4. Hooks/src/App.js
+++ 4. Hooks/src/App.js
@@ -9,7 +9,29 @@ function App() {
   const [textVisible, setVisibility] = useState(true);
 
   // Ex 3
-  let [boxColor, setBoxColor] = useState("green");
+  const [boxColor, setBoxColor] = useState("green");
+
+  // Ex 4 - Todo List
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState({});
+
+  const handleChange = (event) => {
+    setNewTask(event.target.value);
+  };
+
+  const addTask = () => {
+    const task = {
+      id: todoList.length === 0 ? 1 : todoList.length + 1,
+      taskName: newTask,
+    };
+
+    setTodoList([...todoList, task]);
+  };
+
+  const deleteTask = (id) => {
+    console.log(id);
+    setTodoList(todoList.filter((task) => task.id !== id));
+  };
 
   return (
     <div className="App">
@@ -64,13 +86,33 @@ function App() {
         <div style={{ backgroundColor: boxColor }}>This is a box</div>
         <button
           onClick={() => {
-            boxColor = boxColor === "green" ? "red" : "green";
-
-            setBoxColor(boxColor);
+            setBoxColor(boxColor === "green" ? "red" : "green");
           }}
         >
           Change Color
         </button>
+      </section>
+
+      <section>
+        <h1 className="exercise-title">Exercise 4</h1>
+        <input onChange={handleChange}></input>
+        <button onClick={addTask}>Add</button>
+        <div>
+          <ul>
+            {todoList.map((task) => (
+              <div>
+                <li>{task.taskName}</li>
+                <button
+                  onClick={() => {
+                    deleteTask(task.id);
+                  }}
+                >
+                  X
+                </button>
+              </div>
+            ))}
+          </ul>
+        </div>
       </section>
     </div>
   );
