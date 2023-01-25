8ccf7b470d213b9430f57a788cc2cc709a21f97a
a-shiro <alekmitov20@gmail.com>
2023-01-25 17:14:07 +0200

    Refactored Code

--- exercises/src/App.js
+++ exercises/src/App.js
@@ -1,7 +1,37 @@
 import "./App.css";
+import { ExerciseSection } from "./components/ExerciseSection";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <label>Select Exercise</label>
+      <select
+        onChange={(event) => {
+          document.querySelectorAll("section").forEach((section) => {
+            section.className = "";
+            section.classList.add("hidden");
+          });
+
+          const section = document.getElementById(event.target.value);
+          const visibility = section.classList[0];
+
+          section.classList.remove(visibility);
+          section.classList.add(visibility === "hidden" ? "visible" : "hidden");
+        }}
+      >
+        <option>---</option>
+        <option>1</option>
+        <option>2</option>
+        <option>3</option>
+        <option>4</option>
+        <option>5</option>
+        <option>6</option>
+        <option>7</option>
+      </select>
+
+      <ExerciseSection />
+    </div>
+  );
 }
 
 export default App;
