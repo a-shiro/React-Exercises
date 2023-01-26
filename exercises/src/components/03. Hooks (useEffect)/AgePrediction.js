a5a6080c3b66fb546bb95d2ad69aebcb9c67617e
a-shiro <alekmitov20@gmail.com>
2023-01-26 12:42:13 +0200

    Added Exercise

--- exercises/src/components/03. Hooks (useEffect)/AgePrediction.js
+++ exercises/src/components/03. Hooks (useEffect)/AgePrediction.js
@@ -1,1 +1,29 @@
-export const AgePrediction = () => {};
+import { useState, useEffect } from "react";
+
+export const AgePrediction = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [predictedAge, setPredictedAge] = useState(0);
+
+  const fetchData = () => {
+    fetch(`https://api.agify.io?name=${inputValue}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setPredictedAge(data.age);
+      });
+  };
+
+  return (
+    <div>
+      <input
+        placeholder="Ex. Josh"
+        onChange={(event) => {
+          setInputValue(event.target.value);
+        }}
+      />
+      <button onClick={fetchData}>Show Results</button>
+      <div>
+        <h1>Predicted Age: {predictedAge}</h1>
+      </div>
+    </div>
+  );
+};
