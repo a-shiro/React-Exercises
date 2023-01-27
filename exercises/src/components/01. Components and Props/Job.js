86583758553792a0677fbb2e71051524ea546428
a-shiro <alekmitov20@gmail.com>
2023-01-27 15:35:47 +0200

    Refactored Code

--- exercises/src/components/01. Components and Props/Job.js
+++ exercises/src/components/01. Components and Props/Job.js
@@ -1,9 +1,9 @@
 export const Job = (props) => {
   return (
     <div>
-      <h1>{props.salary}</h1>
-      <h1>{props.position}</h1>
-      <h1>{props.company}</h1>
+      <h1>Salary: {props.salary}</h1>
+      <h1>Position: {props.position}</h1>
+      <h1>Company: {props.company}</h1>
     </div>
   );
 };
--- exercises/src/components/01. Components and Props/User.js
+++ exercises/src/components/01. Components and Props/User.js
@@ -1,9 +1,9 @@
 export const User = (props) => {
   return (
     <div>
-      <h2>{props.name}</h2>
-      <h2>{props.age}</h2>
-      <h2>{props.email}</h2>
+      <h2>Name: {props.name}</h2>
+      <h2>Age: {props.age}</h2>
+      <h2>Email: {props.email}</h2>
     </div>
   );
-}
+};
