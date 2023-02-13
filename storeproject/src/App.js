65156b486294b6d234a574873320de5ffa8d61cc
a-shiro <alekmitov20@gmail.com>
2023-02-13 08:53:31 +0200

    Refacored Code

--- storeproject/src/App.js
+++ storeproject/src/App.js
@@ -1,8 +1,25 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
+import { Home } from "./pages/home";
+import { Store } from "./pages/store";
+import { PageNotFount } from "./pages/404";
 
 function App() {
-  return <Navbar />;
+  return (
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/login" element={<PageNotFount />} />
+          <Route path="/register" element={<PageNotFount />} />
+          <Route path="*" element={<PageNotFount />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
--- storeproject/src/components/Navbar.js
+++ storeproject/src/components/Navbar.js
@@ -1,25 +1,12 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import { Home } from "../pages/home";
-import { Store } from "../pages/store";
-import { PageNotFount } from "../pages/404";
+import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   return (
     <nav>
-      <BrowserRouter>
-        <Link to="/">Home</Link>
-        <Link to="/store">Store</Link>
-        <Link to="/login">Login</Link>
-        <Link to="/register">Register</Link>
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/store" element={<Store />} />
-          <Route path="/login" element={<PageNotFount />} />
-          <Route path="/register" element={<PageNotFount />} />
-          <Route path="*" element={<PageNotFount />} />
-        </Routes>
-      </BrowserRouter>
+      <Link to="/">Home</Link>
+      <Link to="/store">Store</Link>
+      <Link to="/login">Login</Link>
+      <Link to="/register">Register</Link>
     </nav>
   );
 };
