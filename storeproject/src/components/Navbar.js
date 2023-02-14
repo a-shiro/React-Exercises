3e17ce7ba1b75a1653796848ea363e4e91419b0a
a-shiro <alekmitov20@gmail.com>
2023-02-14 09:58:57 +0200

    Added Logout Functionality

--- storeproject/src/components/Navbar.js
+++ storeproject/src/components/Navbar.js
@@ -1,16 +1,25 @@
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { AppContext } from "../App";
+import { auth } from "../config/firebase";
+import { signOut } from "firebase/auth";
 
 export const Navbar = () => {
-  const { currentUser } = useContext(AppContext);
+  const { currentUser, setCurrentUser } = useContext(AppContext);
 
   return (
     <div>
       <Link to="/">Home</Link>
       <Link to="/store">Store</Link>
       <Link to="/login">Login</Link>
-      <Link to="/register">Register</Link>
+      <Link
+        onClick={async () => {
+          signOut(auth);
+          setCurrentUser({});
+        }}
+      >
+        Logout
+      </Link>
       {currentUser?.displayName && (
         <div>
           <h3>{currentUser.displayName}</h3>
