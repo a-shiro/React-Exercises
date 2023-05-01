4745b0140d6fe717a4092c3777c010af4643b6c7
a-shiro <alekmitov20@gmail.com>
2023-05-01 12:19:18 +0300

    ---

--- dummyapp/src/App.js
+++ dummyapp/src/App.js
@@ -1,6 +1,9 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import heroImage from "./images/lambo.jpg";
+import heroImageTwo from "./images/paris.jpg";
+import heroImageThree from "./images/rarri.jpg";
+
 import "./App.css";
 
 function App() {
@@ -59,6 +62,8 @@ function App() {
             </div>
           </div>
           <img className="hero-image" src={heroImage} />
+          <img className="hero-image-2" src={heroImageTwo} />
+          <img className="hero-image-3" src={heroImageThree} />
         </div>
       </section>
 
