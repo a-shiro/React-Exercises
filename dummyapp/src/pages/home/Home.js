bd8a63fac0def8a4dde736270cd1ca2debb41514
a-shiro <alekmitov20@gmail.com>
2023-05-08 11:30:51 +0300

    Add: Responsive cards"

--- dummyapp/src/pages/home/Home.js
+++ dummyapp/src/pages/home/Home.js
@@ -29,12 +29,28 @@ const Home = () => {
       <section className="popular-section">
         <div>
           <h1>Popular Choices</h1>
-          <div className="card-slider">
+          <div className="card-wrapper">
             <div className="card">
-              <span className="card-title">Porsche</span>
+              <span className="card-title">Porsche 992 Cabrio --</span>
               <img
                 className="card-image"
                 src="https://res.cloudinary.com/unix-center/image/upload/c_fill,dpr_auto,f_auto,fl_progressive,g_center,h_125,q_auto,w_315/v1580913291/ozwawbomlmkclzllldx1.png"
+              />
+            </div>
+
+            <div className="card medium-screen">
+              <span className="card-title">Lamborghini Hurracan EVO --</span>
+              <img
+                className="card-image"
+                src="https://res.cloudinary.com/unix-center/image/upload/c_fill,dpr_auto,f_auto,fl_progressive,g_center,h_125,q_auto,w_315/v1573743772/tfgvvsuaydikw5ad1yds.png"
+              />
+            </div>
+
+            <div className="card large-screen">
+              <span className="card-title">Mercedes-Benz G63 AMG --</span>
+              <img
+                className="card-image"
+                src="https://res.cloudinary.com/unix-center/image/upload/c_lfill,dpr_auto,f_auto,fl_progressive,g_center,h_108,q_auto,w_270/v1520083160/dyneukcbulzxewb1ay5g.png"
               />
             </div>
           </div>
