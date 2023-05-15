34f1449c53e533d6b132c6e834f807940f6ea3a9
a-shiro <alekmitov20@gmail.com>
2023-05-15 09:44:10 +0300

    ---

--- dummyapp/src/App.js
+++ dummyapp/src/App.js
@@ -47,6 +47,31 @@ function App() {
       </nav>
 
       <Home />
+
+      <footer>
+        <div className="info-wrapper">
+          <div>
+            <h6 className="info-title">Contacts:</h6>
+            <p>+359 88 888 8888</p>
+            <p>rentx@email.com</p>
+          </div>
+          <div>
+            <h6 className="info-title">Social Networking:</h6>
+            <p>Instagram - @official_rentx</p>
+            <p>Facebook - RentX Cars</p>
+            <p>TikTok - @official_rentx</p>
+          </div>
+          <div>
+            <h6 className="info-title">Links:</h6>
+            <p>Terms And Conditions</p>
+            <p>Trustpilot Reviews</p>
+            <p>FAQ</p>
+          </div>
+        </div>
+        <div className="copyright-wrapper">
+          <p>Copyright © 2023 RentX. All rights reserved</p>
+        </div>
+      </footer>
     </div>
   );
 }
