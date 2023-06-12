ae5c555c8d14232ca1971ac218b5f58066b60519
a-shiro <alekmitov20@gmail.com>
2023-06-12 09:49:02 +0300

    ---

--- dummyapp/src/pages/about/About.js
+++ dummyapp/src/pages/about/About.js
@@ -4,9 +4,10 @@ import historyIcon from "../../images/history.png";
 import carsIcon from "../../images/cars.png";
 import locationIcon from "../../images/locations.png";
 import customerServiceIcon from "../../images/customer-service.png";
-import aboutImage from "../../images/mclaren.jpg";
 import companyHistoryImage from "../../images/company-history.jpg";
 import carBrandsImage from "../../images/car-brands.png";
+import serviceImage from "../../images/service.jpg";
+import locationsImage from "../../images/locations.jpg";
 import { useRef } from "react";
 
 const About = () => {
@@ -18,14 +19,6 @@ const About = () => {
   return (
     <main>
       <section className="hero-section-about">
-        <div className="hero-titles-wrapper-about">
-          <h1>Your gateway to unparalleled luxury</h1>
-          <p>
-            Unmatched elegance and performance with our premium car rental
-            services
-          </p>
-        </div>
-
         <div className="hero-cards-wrapper-about">
           <HeroCard
             title="History"
@@ -139,49 +132,49 @@ const About = () => {
                 the perfect harmony of power, grace, and timeless design.
               </li>
               <li>
-                <span>Bentley</span>: Renowned for their handcrafted interiors
+                <span>Bentley:</span> Renowned for their handcrafted interiors
                 and exceptional comfort, Bentley cars provide an unparalleled
                 level of opulence. From grand touring models to powerful sports
                 cars, driving a Bentley is an experience reserved for those who
                 appreciate the finest things in life.
               </li>
               <li>
-                <span>Ferrari</span>: Embrace the passion and adrenaline of
+                <span>Ferrari:</span> Embrace the passion and adrenaline of
                 Italian automotive excellence with a Ferrari rental. These
                 iconic sports cars combine breathtaking design, cutting-edge
                 technology, and blistering performance, delivering an unmatched
                 thrill on the road.
               </li>
               <li>
-                <span>Lamborghini</span>: Rent a Lamborghini to unleash the raw
+                <span>Lamborghini:</span> Rent a Lamborghini to unleash the raw
                 power and striking aesthetics of these legendary supercars. With
                 their distinct angular lines and roaring engines, Lamborghinis
                 turn heads wherever they go, making a bold statement of
                 unrivaled performance.
               </li>
               <li>
-                <span>Mercedes-Benz</span>: A symbol of sophistication and
+                <span>Mercedes-Benz:</span> A symbol of sophistication and
                 innovation, Mercedes-Benz offers a diverse range of luxury
                 vehicles catering to various tastes and needs. From luxurious
                 sedans to powerful SUVs, Mercedes-Benz combines elegance with
                 cutting-edge technology.
               </li>
               <li>
-                <span>Porsche</span>: Experience the perfect balance between
+                <span>Porsche:</span> Experience the perfect balance between
                 performance and comfort with a Porsche rental. These
                 German-engineered sports cars offer precision handling,
                 thrilling acceleration, and unmistakable style, making every
                 drive an exhilarating adventure.
               </li>
               <li>
-                <span>Rolls-Royce</span>: Renting a Rolls-Royce is the epitome
+                <span>Rolls-Royce:</span> Renting a Rolls-Royce is the epitome
                 of refined luxury. Known for their unparalleled craftsmanship,
                 attention to detail, and iconic Spirit of Ecstasy hood ornament,
                 Rolls-Royce cars deliver a regal and serene driving experience
                 like no other.
               </li>
               <li>
-                <span>Maserati</span>: Embodying Italian elegance and
+                <span>Maserati:</span> Embodying Italian elegance and
                 exhilaration, Maserati cars blend luxury and performance
                 seamlessly. From seductive grand tourers to dynamic sports cars,
                 Maserati captures the essence of Italian flair and driving
@@ -205,99 +198,164 @@ const About = () => {
         />
       </section>
 
-      <section className="about-section" ref={refService}>
-        <div className="about-wrapper">
-          <div className="about-text-wrapper">
-            <h1>Service --</h1>
+      <section className="info-section-about" ref={refService}>
+        <div className="info-wrapper-about">
+          <h1>Service --</h1>
+          <div>
             <p>
-              At <span>RentX</span>, we believe that a remarkable journey begins
-              with an extraordinary vehicle, and we take pride in curating a
-              fleet of the most exclusive and coveted automobiles in the world.
-            </p>
-            <p>
-              With a deep passion for automotive excellence, our team at{" "}
-              <span>RentX</span> strives to deliver an unforgettable experience
-              to every customer. We understand that driving is not merely a
-              means of transportation; it is an expression of individuality and
-              a reflection of personal success. Whether you require a vehicle
-              for a special occasion, a business trip, or simply to indulge in a
-              moment of pure indulgence, we have the perfect luxury car waiting
-              for you.
-            </p>
-            <p>
-              Our fleet comprises a carefully selected collection of the most
-              iconic and coveted automobiles, including prestigious brands like{" "}
-              <span>
-                Rolls-Royce, Bentley, Lamborghini, Ferrari, Aston Martin
-              </span>
-              , and many more. Each vehicle is meticulously maintained to ensure
-              optimum performance and immaculate presentation, guaranteeing a
-              seamless and exhilarating driving experience.
-            </p>
-            <p>
-              Indulge your passion for luxury and elevate your driving
-              experience with <span>RentX</span>. Discover the pleasure of
-              commanding the finest automobiles ever created and unlock a world
-              of unforgettable memories. Welcome to the epitome of automotive
-              luxury. Welcome to <span>RentX</span>.
+              At <span>RentX</span>, we understand that true luxury knows no
+              bounds when it comes to time and convenience. That's why we offer
+              our esteemed clientele a dedicated 24/7 service that ensures a
+              seamless and extraordinary experience, no matter when or where you
+              require our assistance.
             </p>
 
-            <p className="contacts-paragraph">
-              Feel free to give us a <a href="#">call</a> at any time or come to
-              a RentX <a href="#">office</a> of your choice. Our call center
-              supports 5 languages ​​(EN - DE - ES - IT - RU) and we can advise
-              you on the car that suits you best.
+            <ul>
+              <li>
+                <span>Around-the-Clock Support:</span> Our team of knowledgeable
+                and professional staff is available 24 hours a day, 7 days a
+                week, ready to assist you with any inquiries, reservations, or
+                concerns you may have. Whether you need assistance with choosing
+                the perfect vehicle, modifying your booking, or seeking
+                recommendations, our experts are just a phone call or email
+                away.
+              </li>
+              <li>
+                <span>Flexible Reservations:</span> With our 24/7 service, you
+                have the freedom to make reservations at your convenience.
+                Whether you're planning ahead or require a last-minute rental,
+                our flexible reservation system allows you to secure the luxury
+                car of your dreams whenever you need it, ensuring a stress-free
+                and seamless experience.
+              </li>
+              <li>
+                <span>On-Demand Delivery:</span> We understand that time is a
+                precious commodity, and convenience is paramount. With our
+                service, we offer on-demand delivery and pickup of your rental
+                vehicle. Whether you prefer to have the car delivered to your
+                doorstep, hotel, or any other location of your choosing, our
+                dedicated team will ensure that your luxury car is delivered
+                promptly and in immaculate condition.
+              </li>
+              <li>
+                <span>Roadside Assistance:</span> Your safety and peace of mind
+                are of utmost importance to us. In the unlikely event of any
+                issues or emergencies during your rental, our 24/7 service
+                includes comprehensive roadside assistance. Whether it's a flat
+                tire, a battery jump-start, or any other unexpected situation,
+                our support team will promptly assist you, ensuring that your
+                journey continues uninterrupted.
+              </li>
+              <li>
+                <span>Concierge Services:</span> <span>RentX</span> goes beyond
+                just providing luxury cars; we strive to create exceptional
+                experiences. Our services are available to assist you with
+                various arrangements, including restaurant reservations, event
+                tickets, and more. Let us take care of the finer details,
+                allowing you to focus on enjoying your luxury driving experience
+                to the fullest.
+              </li>
+              <li>
+                <span>Global Reach:</span> Whether you're traveling locally or
+                exploring a new destination, our 24/7 service extends worldwide.
+                With our extensive network of affiliates and partners, we can
+                arrange luxury car rentals in numerous cities and countries,
+                ensuring that your unparalleled driving experience transcends
+                borders.
+              </li>
+            </ul>
+            <p>
+              At <span>RentX</span>, our commitment to delivering excellence
+              knows no time constraints. With our 24/7 service, we are dedicated
+              to providing unparalleled support, convenience, and peace of mind
+              throughout your entire rental journey. Experience luxury on your
+              terms, anytime, anywhere.
             </p>
           </div>
-          <img src={aboutImage} />
         </div>
+        <img className="brands-image" src={serviceImage} alt="section-image" />
       </section>
-      <section className="about-section" ref={refLocation}>
-        <div className="about-wrapper">
-          <div className="about-text-wrapper">
-            <h1>Locations --</h1>
+
+      <section className="info-section-about" ref={refLocation}>
+        <div className="info-wrapper-about">
+          <h1>Locations --</h1>
+          <div>
             <p>
-              At <span>RentX</span>, we believe that a remarkable journey begins
-              with an extraordinary vehicle, and we take pride in curating a
-              fleet of the most exclusive and coveted automobiles in the world.
-            </p>
-            <p>
-              With a deep passion for automotive excellence, our team at{" "}
-              <span>RentX</span> strives to deliver an unforgettable experience
-              to every customer. We understand that driving is not merely a
-              means of transportation; it is an expression of individuality and
-              a reflection of personal success. Whether you require a vehicle
-              for a special occasion, a business trip, or simply to indulge in a
-              moment of pure indulgence, we have the perfect luxury car waiting
-              for you.
-            </p>
-            <p>
-              Our fleet comprises a carefully selected collection of the most
-              iconic and coveted automobiles, including prestigious brands like{" "}
-              <span>
-                Rolls-Royce, Bentley, Lamborghini, Ferrari, Aston Martin
-              </span>
-              , and many more. Each vehicle is meticulously maintained to ensure
-              optimum performance and immaculate presentation, guaranteeing a
-              seamless and exhilarating driving experience.
-            </p>
-            <p>
-              Indulge your passion for luxury and elevate your driving
-              experience with <span>RentX</span>. Discover the pleasure of
-              commanding the finest automobiles ever created and unlock a world
-              of unforgettable memories. Welcome to the epitome of automotive
-              luxury. Welcome to <span>RentX</span>.
+              At <span>RentX</span>, we understand that true luxury knows no
+              bounds when it comes to time and convenience. That's why we offer
+              our esteemed clientele a dedicated 24/7 service that ensures a
+              seamless and extraordinary experience, no matter when or where you
+              require our assistance.
             </p>
 
-            <p className="contacts-paragraph">
-              Feel free to give us a <a href="#">call</a> at any time or come to
-              a RentX <a href="#">office</a> of your choice. Our call center
-              supports 5 languages ​​(EN - DE - ES - IT - RU) and we can advise
-              you on the car that suits you best.
+            <ul>
+              <li>
+                <span>Around-the-Clock Support:</span> Our team of knowledgeable
+                and professional staff is available 24 hours a day, 7 days a
+                week, ready to assist you with any inquiries, reservations, or
+                concerns you may have. Whether you need assistance with choosing
+                the perfect vehicle, modifying your booking, or seeking
+                recommendations, our experts are just a phone call or email
+                away.
+              </li>
+              <li>
+                <span>Flexible Reservations:</span> With our 24/7 service, you
+                have the freedom to make reservations at your convenience.
+                Whether you're planning ahead or require a last-minute rental,
+                our flexible reservation system allows you to secure the luxury
+                car of your dreams whenever you need it, ensuring a stress-free
+                and seamless experience.
+              </li>
+              <li>
+                <span>On-Demand Delivery:</span> We understand that time is a
+                precious commodity, and convenience is paramount. With our
+                service, we offer on-demand delivery and pickup of your rental
+                vehicle. Whether you prefer to have the car delivered to your
+                doorstep, hotel, or any other location of your choosing, our
+                dedicated team will ensure that your luxury car is delivered
+                promptly and in immaculate condition.
+              </li>
+              <li>
+                <span>Roadside Assistance:</span> Your safety and peace of mind
+                are of utmost importance to us. In the unlikely event of any
+                issues or emergencies during your rental, our 24/7 service
+                includes comprehensive roadside assistance. Whether it's a flat
+                tire, a battery jump-start, or any other unexpected situation,
+                our support team will promptly assist you, ensuring that your
+                journey continues uninterrupted.
+              </li>
+              <li>
+                <span>Concierge Services:</span> <span>RentX</span> goes beyond
+                just providing luxury cars; we strive to create exceptional
+                experiences. Our services are available to assist you with
+                various arrangements, including restaurant reservations, event
+                tickets, and more. Let us take care of the finer details,
+                allowing you to focus on enjoying your luxury driving experience
+                to the fullest.
+              </li>
+              <li>
+                <span>Global Reach:</span> Whether you're traveling locally or
+                exploring a new destination, our 24/7 service extends worldwide.
+                With our extensive network of affiliates and partners, we can
+                arrange luxury car rentals in numerous cities and countries,
+                ensuring that your unparalleled driving experience transcends
+                borders.
+              </li>
+            </ul>
+            <p>
+              At <span>RentX</span>, our commitment to delivering excellence
+              knows no time constraints. With our 24/7 service, we are dedicated
+              to providing unparalleled support, convenience, and peace of mind
+              throughout your entire rental journey. Experience luxury on your
+              terms, anytime, anywhere.
             </p>
           </div>
-          <img src={aboutImage} />
         </div>
+        <img
+          className="brands-image"
+          src={locationsImage}
+          alt="section-image"
+        />
       </section>
     </main>
   );
